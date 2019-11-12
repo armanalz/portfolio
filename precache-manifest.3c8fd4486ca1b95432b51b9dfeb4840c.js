11ad13278bb7a4b0ae5a5c3fa81bb8fecf9e42f3
armanalz <armanalizadeh467@gmail.com>
2019-11-12 19:31:19 +0330

    Updates

---
+++ precache-manifest.3c8fd4486ca1b95432b51b9dfeb4840c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1185f6d14eabc1e1a37cc975f6100ab",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6e4727ff5b3de7702d61",
+    "url": "/portfolio/static/css/main.ba0b3da5.chunk.css"
+  },
+  {
+    "revision": "90c53f537dd78b514a80",
+    "url": "/portfolio/static/js/2.09a82e4d.chunk.js"
+  },
+  {
+    "revision": "6e4727ff5b3de7702d61",
+    "url": "/portfolio/static/js/main.d2c716cf.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  }
+]);
